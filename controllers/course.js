5ff46c89f841be63d9395b605d30d4c61d447f89
Teemu Koivisto <tkoivisto456@gmail.com>
2017-08-17 23:40:46 +0300

    Change url for getting courses

--- controllers/course.js
+++ controllers/course.js
@@ -5,9 +5,10 @@ module.exports = {
   async getAll(req, res, next) {
     try {
       const courses = await RequestService.get({
-        url: "opas=5323",
+        // url: "opas=5323",
+        url: "nimiTaiTunniste=TKT&opas=5323&lukukausi=135",        
       })
-      res.json(courses)
+      res.json({ courses, })
     } catch (err) {
       next(err)
     }
