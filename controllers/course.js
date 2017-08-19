dd85a127be6551de81d2f471419a884c1a0b0cf5
Teemu Koivisto <tkoivisto456@gmail.com>
2017-08-19 20:48:52 +0300

    Fetch all TKTL courses

--- controllers/course.js
+++ controllers/course.js
@@ -4,13 +4,16 @@ const RequestService = require("../services/RequestService")
 module.exports = {
   async getAll(req, res, next) {
     try {
+      const courses = await RequestService.get({
+        url: "vastuuorganisaatio=1000000921", // on koko TKTn laitos
+      })
       const bachelorCourses = await RequestService.get({
         url: "opas=5323",
       })
       const masterCourses = await RequestService.get({
         url: "opas=5351",
       })
-      res.json({ bachelorCourses, masterCourses, })
+      res.json({ courses, bachelorCourses, masterCourses, })
     } catch (err) {
       next(err)
     }
