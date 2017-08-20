38e38f6c657de145c34220b36199dc0ff77c923e
Teemu Koivisto <tkoivisto456@gmail.com>
2017-08-20 13:18:05 +0300

    Add id and opintoniLink to courses

--- controllers/course.js
+++ controllers/course.js
@@ -2,11 +2,15 @@
 const RequestService = require("../services/RequestService")
 const ScraperService = require("../services/ScraperService")
 
+function joinOodiCoursesToOpintoni(opintoniCourses, oodiCourses) {
+
+}
+
 module.exports = {
   async getAll(req, res, next) {
     try {
       const scrapedCourses = await ScraperService.getTKTCourses()
-      console.log(scrapedCourses.length)
+      console.log(scrapedCourses.filter(course => course.number === "TKT10003"))
       // console.log(scrapedCourses)
       // const courses = await RequestService.get({
       //   url: "vastuuorganisaatio=1000000921", // on koko TKTn laitos
@@ -17,7 +21,8 @@ module.exports = {
       // const masterCourses = await RequestService.get({
       //   url: "opas=5351",
       // })
-      res.json({ courses, bachelorCourses, masterCourses, })
+      // res.json({ courses, bachelorCourses, masterCourses, })
+      res.json(scrapedCourses)
     } catch (err) {
       next(err)
     }
--- services/ScraperService.js
+++ services/ScraperService.js
@@ -1,8 +1,8 @@
 
 const osmosis = require("osmosis")
 
-const TKT_COURSES_1 = "https://courses.helsinki.fi/fi/search/results/field_imp_organisation/tietojenk%C3%A4sittelytieteen-laitos-953/field_imp_organisation/tietojenk%C3%A4sittelytieteen-kandiohjelma-1922/field_imp_organisation/tietojenk%C3%A4sittelytieteen-maisteriohjelma-1929?search=&search=&sorting=title_field%3Aasc&items_per_page=100"
-const TKT_COURSES_2 = "https://courses.helsinki.fi/fi/search/results/field_imp_organisation/tietojenk%C3%A4sittelytieteen-laitos-953/field_imp_organisation/tietojenk%C3%A4sittelytieteen-kandiohjelma-1922/field_imp_organisation/tietojenk%C3%A4sittelytieteen-maisteriohjelma-1929?academic_year=2017%20-%202018&search=&sorting=title_field%3Aasc&items_per_page=100&page=1&order=title_field&sort=asc"
+const TKT_COURSES_1 = "https://courses.helsinki.fi/fi/search/results/field_imp_organisation/tietojenk%C3%A4sittelytieteen-maisteriohjelma-1929/field_imp_organisation/datatieteen-maisteriohjelma-1931/field_imp_organisation/tietojenk%C3%A4sittelytieteen-laitos-953/field_imp_organisation/tietojenk%C3%A4sittelytieteen-kandiohjelma-1922?search=&search=&sorting=title_field%3Aasc&items_per_page=100&order=title_field&sort=asc"
+const TKT_COURSES_2 = "https://courses.helsinki.fi/fi/search/results/field_imp_organisation/tietojenk%C3%A4sittelytieteen-maisteriohjelma-1929/field_imp_organisation/datatieteen-maisteriohjelma-1931/field_imp_organisation/tietojenk%C3%A4sittelytieteen-laitos-953/field_imp_organisation/tietojenk%C3%A4sittelytieteen-kandiohjelma-1922?academic_year=2017%20-%202018&search=&sorting=title_field%3Aasc&items_per_page=100&page=1&order=title_field&sort=asc"
 
 module.exports = {
   getTKTCourses() {
@@ -19,7 +19,8 @@ module.exports = {
       .get(url)
       .find("tbody")
       .set({
-        id: ["tr td.views-field-field-imp-reference-to-courses-field-course-course-number"],
+        link: ["tr a@href"],
+        number: ["tr td.views-field-field-imp-reference-to-courses-field-course-course-number"],
         name: ["tr td.views-field-title-field"],
         type: ["tr td.views-field-field-imp-reference-to-courses-field-course-type-of-teaching"],
         format: ["tr td.views-field-field-imp-method-of-study"],
@@ -27,9 +28,12 @@ module.exports = {
         teacher: ["tr td.views-field-field-imp-teacher"]
       })
       .data(data => {
-        courseData = data.id.map((id, index) => {
+        courseData = data.number.map((number, index) => {
+          const link = data.link[index]
           return {
-            id,
+            id: link ? parseInt(link.substring(link.lastIndexOf('/') + 1)) : null,
+            opintoniLink: link,
+            number,
             name: data.name[index],
             type: data.type[index],
             format: data.format[index],
